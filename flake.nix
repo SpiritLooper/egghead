497d30b4729efc63f1b50d512d42dea12eedd36f
Julien CLAISSE <julienclaisse@orange.fr>
2026-03-31 22:20:47 +0200

    Init ansible

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Flake to handle Skypiea VPS";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "ansible-dev";
+          buildInputs = with pkgs; [
+            age
+            ansible            # Ansible complet
+            ansible-lint       # linting
+            jq
+            sops
+            yq
+            python313
+            python313Packages.jinja2
+          ];
+          shellHook = ''
+            ansible --version
+            exec fish
+          '';
+        };
+      });
+}
